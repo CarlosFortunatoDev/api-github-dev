a0c26304306c4fd0e02c94684b705d00b4463edb
Carlos Fortunato <carlosfortunatodev@gmail.com>
2024-06-05 23:23:07 -0300

    Criando estrutura central da tela de repositórios

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import MainPage from "./pages/MainPage";
+import Repositories from "./pages/RepositoriesPage";
 
 export default function AppRoutes() {
   return (
     <Routes>
+      <Route path="/repositories" element={<Repositories />} />
       <Route path="/" element={<MainPage />} />
     </Routes>
   );
