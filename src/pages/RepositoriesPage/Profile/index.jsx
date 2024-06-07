3e385ae1e0d8a3a138fb9cdf3ea01d909acd613d
Carlos Fortunato <carlosfortunatodev@gmail.com>
2024-06-06 23:26:33 -0300

    Finalizando componente Profile

--- src/pages/RepositoriesPage/Profile/index.jsx
+++ src/pages/RepositoriesPage/Profile/index.jsx
@@ -1,31 +1,54 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
 import {MdGroup, MdLocationCity, MdWork, MdLink} from 'react-icons/md'
 
 import { Container, Header, Avatar, Login, Name, Inner, Data } from './style'
 
-function Profile() {
+function Profile({user}) {
   return (
     <Container>
       <Header>
-        <Avatar src='https://avatars.githubusercontent.com/u/125428271?v=4'/>
-        <Login>carlosfortunatodev</Login>
-        <Name>Carlos Fortunato</Name>
+        <Avatar src={user.avatar_url}/>
+        <Login>{user.login}</Login>
+        <Name>{user.name}</Name>
       </Header>
       <Inner>
         <Data>
           <MdGroup size={20} />
-          10&nbsp;<i>Seguidores</i>&nbsp;&middot;&nbsp;30&nbsp;<i>seguindo</i>
+          {user.followers}&nbsp;<i>Seguidores</i>&nbsp;&middot;&nbsp;{user.following}&nbsp;<i>Seguindo</i>
         </Data>
-        <Data><MdWork size={20} />Embraer</Data>
-        <Data><MdLocationCity size={20} />São José dos Campos</Data>
-        <Data>
-          <MdLink size={20} />
-          <a href="https://embraer.com/" target='_blank' rel="noreferrer">Embraer</a>
-        </Data>
+        {user.company && (
+          <Data>
+            <MdWork size={20} />{user.company}
+          </Data>
+        )}
+        {user.location && (
+          <Data>
+            <MdLocationCity size={20} />{user.location}
+          </Data>
+        )}
+        {user.blog && (
+          <Data>
+            <MdLink size={20} />
+            <a href={user.blog} target='_blank' rel="noreferrer">Blog</a>
+          </Data>
+        )}
       </Inner>
     </Container>
   )
+}
+
+Profile.propTypes ={
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    company: PropTypes.string,
+    blog: PropTypes.string,
+    location: PropTypes.string,
+  }).isRequired,
 }
 
 export default Profile
--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -6,10 +6,21 @@ import Filter from './Filter'
 import Repositories from './Repositories'
 
 function RepositoriesPage() {
+  const user = {
+    login: "CarlosFortunatoDev",
+    name: "Carlos Fortunato",
+    avatar_url: "https://avatars.githubusercontent.com/u/125428271?v=4",
+    followers: 4,
+    following: 14,
+    company: 'UNIP',
+    blog: "https://carlosfortunatodev.github.io/Portfolio-react/",
+    location: "São José dos Campos - SP",
+  }
+
   return (
     <Container>
       <Sidebar>
-        <Profile />
+        <Profile user={user} />
         <Filter/>
       </Sidebar>
       <Main>
